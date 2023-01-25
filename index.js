54f48ae407a2a610c32821e33947bbeaba3af143
faisal12345-hash <83586844+faisal12345-hash@users.noreply.github.com>
2023-01-25 17:08:36 +0530

    Add files via upload
    https://63d114021611b6113a5aa8e0--beautiful-cupcake-e6bce1.netlify.app/

---
+++ index.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('form');
+const todoList = document.querySelector('ul');
+const button = document.querySelector('button');
+const input = document.getElementById('user-todo');
+let todosArray = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+
+localStorage.setItem('todos', JSON.stringify(todosArray));
+let storage = JSON.parse(localStorage.getItem('todos'));
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    todosArray.push(input.value);
+    localStorage.setItem('todos', JSON.stringify(todosArray));
+    todoMaker(input.value);
+    input.value = "";
+});
+
+let todoMaker = function (text) {
+    let todo = document.createElement('li');
+    todo.textContent = text;
+    todoList.appendChild(todo);
+}
+
+for (let i = 0; i < storage.length; i++) {
+    todoMaker(storage[i]);
+}
+
+button.addEventListener('click', function () {
+    localStorage.clear();
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.firstChild);
+    }
+})
